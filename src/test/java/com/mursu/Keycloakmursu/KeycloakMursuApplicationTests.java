329d31cdea49914b444111084136b54bf4fc3e11
Gulshan <shrigulshankumar@gmail.com>
2024-05-13 02:18:16 +0300

    Initial commit

---
+++ src/test/java/com/mursu/Keycloakmursu/KeycloakMursuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mursu.Keycloakmursu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakMursuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
